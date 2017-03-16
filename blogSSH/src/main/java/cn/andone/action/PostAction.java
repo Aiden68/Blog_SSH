0b765f08127f40af758c7619b687a50507b4555f
Aiden68 <jllsjtu@163.com>
2017-03-17 00:21:36 +0800

    -

---
+++ blogSSH/src/main/java/cn/andone/action/PostAction.java
@@ -0,0 +1,16 @@
+package cn.andone.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class PostAction extends ActionSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String list(){
+		System.out.println("Œ“¿¥¡À");
+		return "frontList";
+	}
+}
